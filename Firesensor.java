136c9ccd73eb315a961ee1a79833c01ce9fa2f75
Jawaharm444 <jawaharm444@gmail.com>
2025-10-31 12:05:01 +0530

    Implement FireSensor class with fire detection method

---
+++ Firesensor.java
@@ -0,0 +1,12 @@
+public class FireSensor {
+    public void detectFire() {
+        // Simulating fire detection
+        boolean fireDetected = true;
+
+        if (fireDetected) {
+            System.out.println("Fire detected!");
+        } else {
+            System.out.println("No fire detected.");
+        }
+    }
+}
